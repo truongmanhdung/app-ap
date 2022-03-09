70b12e89821ca14fd6fbe42b599887e408a819f2
truongmanhdung <dungtmph12934@fpt.edu.vn>
2022-03-09 18:04:12 +0700

    setup project ap

---
+++ App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import persistor, { store } from './src/app/store';
+import HomeScreen from './src/screens/HomeScreen';
+
+import LoginScreen from './src/screens/LoginScreen';
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+   },
+ });
+ const Stack = createNativeStackNavigator();
+ const App = () => {
+   return (
+     <Provider store={store}>
+       <PersistGate loading={null} persistor={persistor}>
+         <View style={styles.container}>
+           <NavigationContainer>
+             <Stack.Navigator>
+               <Stack.Screen
+                 name="Home"
+                 component={HomeScreen}
+                 options={{headerShown: false}}
+               />
+               <Stack.Screen  options={{headerShown: false}} name="Login" component={LoginScreen} /> 
+             </Stack.Navigator>
+           </NavigationContainer>
+         </View>
+       </PersistGate>
+     </Provider>
+   );
+ };
+ 
+ export default App;
+ 
+ 
